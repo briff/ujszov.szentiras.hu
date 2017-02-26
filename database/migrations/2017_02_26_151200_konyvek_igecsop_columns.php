611724addc1d4ab409a1a1ed4b4ae159a2f2e7d8
briff <fodber@organum.hu>
2017-02-26 15:15:38 +0100

    Adhere to new input file structure

--- database/migrations/2017_02_26_151200_konyvek_igecsop_columns.php
+++ database/migrations/2017_02_26_151200_konyvek_igecsop_columns.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class KonyvekUseNewOrderColumns extends Migration
+class KonyvekIgecsop extends Migration
 {
     /**
      * Run the migrations.
